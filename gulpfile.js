a4000dc09b8afe46dbf802e9fc2f0a048576050b
mnemonic101 <mnemonic@codemonic.net>
2016-08-31 18:46:15 +0200

    npm tasks added

--- gulpfile.js
+++ gulpfile.js
@@ -88,7 +88,7 @@ gulp.task("build", function (cb) {
   );
 });
 
-gulp.task("tests", ["build"], function () {
+gulp.task("tests", ["build-spec"], function () {
   return gulp.src("./")
     .pipe(jasmine({ config: require("./spec/support/jasmine.json"), includeStackTrace: true, verbose: true }));
 });
